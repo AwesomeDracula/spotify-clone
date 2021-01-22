13e589bf49f39f565a267a6872288af8c2db0e44
AwesomeDracula <trungku32@gmail.com>
2021-01-22 15:11:31 +0700

    initial app and login page

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useEffect ,useState} from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './Spotify';
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token){
+      setToken(_token); 
+    }
+  }, [])
+
+  return (
+    <div className="app">
+      {
+        token ? (
+          <h1>Logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
+      
+
+    </div>
+  );
+}
+
+export default App;
